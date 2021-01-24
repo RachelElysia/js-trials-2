e62dcc6b0a80961b63dd3369da437b4aa2124dba
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-01-24 18:02:29 +0000

    Finish functions.js Functions, Build functions.js Doctests

--- functions.js
+++ functions.js
@@ -2,15 +2,65 @@
 
 
 // 1. isHometown
+function isHometown(town) {
+    /*
 
-// Define your function here
+    > isHometown('San Francisco')
+    false
+    > isHometown('San Jose')
+    true
 
+    */
+
+    return (town === 'San Jose')
+}
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    /*
 
+    > getFullName('Rachel', 'Perkins')
+    'Rachel Perkins'
+
+    */
+
+    return (firstName + " " + lastName)
+}
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05) {
+    /*
+
+    > calculateTotal(100, 'MA')
+    106
+    > calculateTotal(100, 'CA', 0.07)
+    110.21
+    > calculateTotal(100, 'WA', 0.04)
+    104
+    
+    */
+    let subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+
+    if (state == 'CA') {
+        fee = 0.03 * subtotal;
+    }
+    if (state == 'PA') {
+        fee = 2;
+    }
+    if (state == 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        }
+        else {
+            fee = 3;
+        }
+    }
+
+    return subtotal + fee
+    }
+
+
